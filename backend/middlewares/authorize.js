75b7fd4d28e72140e48fde7ec2fc8f2bce6d8518
Indrek Anissimov <indrek.anissimov@gmail.com>
2020-12-13 21:20:45 +0200

    Bonus Task

--- backend/middlewares/authorize.js
+++ backend/middlewares/authorize.js
@@ -1,4 +1,5 @@
 const UserModel = require('../models/UserModel');
+const jwt = require('../library/jwt');
 
 module.exports = (request, response, next) => {
 
@@ -10,7 +11,10 @@ module.exports = (request, response, next) => {
     */
 
     if (request.headers.authorization) {
-        UserModel.getById(1, (user) => {
+
+        let token = request.headers.authorization.split(" ")[1];
+
+        UserModel.getById(jwt.verifyAccessToken(token).id, (user) => {
             request.currentUser = user;
             next();
         });
